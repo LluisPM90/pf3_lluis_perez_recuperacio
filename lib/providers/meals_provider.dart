d91d906e1010e997160f5ea19678fc7d7c874c66
LluisPM90 <lluisjaumeperez@paucasesnoves>
2026-05-30 12:59:33 +0200

    Actualitzacio PF3

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -6,14 +6,27 @@ import 'package:http/http.dart' as http;
 import '../models/meal.dart';
 import '../models/meals_response.dart';
 
+/// Provider encarregat de gestionar l'estat de l'aplicació.
+///
+/// Centralitza la lògica de negoci i el consum de l'API.
+/// La interfície només consumeix les dades que proporciona
+/// aquesta classe.
 class MealsProvider extends ChangeNotifier {
 
+  /// Llista de plats obtinguts des de TheMealDB.
   List<Meal> meals = [];
 
+  /// Constructor.
+  /// Quan es crea el Provider es carreguen automàticament
+  /// les dades de l'API.
   MealsProvider() {
     getMeals();
   }
 
+  /// Realitza una petició HTTP a TheMealDB.
+  ///
+  /// Converteix la resposta JSON en objectes Meal i
+  /// actualitza l'estat de l'aplicació.
   Future<void> getMeals() async {
 
     final url = Uri.parse(
@@ -26,8 +39,11 @@ class MealsProvider extends ChangeNotifier {
       json.decode(response.body),
     );
 
+    // Guardam els plats obtinguts de l'API.
     meals = mealsResponse.meals;
 
+    // Notificam als widgets que l'estat ha canviat
+    // perquè es reconstruïsquen automàticament.
     notifyListeners();
   }
 }
